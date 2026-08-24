510dbfe5195cf00679ecb147bce95e20a3daaff7
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-24 13:23:06 +0800

    fix: apply model patch, add acceptance test

--- internal/clock/window.go
+++ internal/clock/window.go
@@ -39,7 +39,7 @@ func NewPreheatWindow(clk ProcessClock) *PreheatWindow {
 }
 
 func (p *PreheatWindow) Ready(startedAt time.Time) bool {
-	return time.Since(startedAt) >= model.PreheatWindow
+	return p.window.Satisfied(p.clk, startedAt)
 }
 
 func (p *PreheatWindow) Require(startedAt time.Time) error {
